a732418da33d6fefe257208d5aa4b76d89ef95b9
klu2300033153 <2300033153@kluniversity.in>
2025-10-25 08:23:19 +0530

    Fixed base name

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/carrental">
       <Navbar />
       <Routes>
         <Route path="/" element={<Login />} />
